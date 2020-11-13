90d07e719523f79d53214241104155d0648e432e
Matthew Larson <maflarson@gmail.com>
2020-11-13 10:24:54 -0700

    got a bunch done

--- index.js
+++ index.js
@@ -87,7 +87,7 @@ const zooAnimals = [
   /* 🦁🦁🦁 Request 4: .reduce() 🦁🦁🦁
   The zoos need to know their total animal population across the United States. Find the total population from all the zoos using the .reduce() method. Remember the reduce method takes two arguments: a callback (which itself takes two args), and an initial value for the count.
   */
-  function USApop(){
+  function USApop(array){
     
 
   }
